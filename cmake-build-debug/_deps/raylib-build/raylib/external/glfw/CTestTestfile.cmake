bcf02868d2064a13b5f92e5977840d52a6ae7bdf
ziidonato <zii.donato@outlook.com>
2023-08-30 19:52:48 -0400

    removed cmake-build-debug; added multithreading, need to rethink structure

--- cmake-build-debug/_deps/raylib-build/raylib/external/glfw/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ziido/CLionProjects/game_of_life/cmake-build-debug/_deps/raylib-src/src/external/glfw
-# Build directory: C:/Users/ziido/CLionProjects/game_of_life/cmake-build-debug/_deps/raylib-build/raylib/external/glfw
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- cmake-build-debug/_deps/raylib-build/raylib/external/glfw/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ziido/CLionProjects/game_of_life/cmake-build-debug/_deps/raylib-src/src/external/glfw/src
-# Build directory: C:/Users/ziido/CLionProjects/game_of_life/cmake-build-debug/_deps/raylib-build/raylib/external/glfw/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
