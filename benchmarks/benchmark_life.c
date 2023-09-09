0d3149f7e1872c4ae31cc7540bf0307ff9b48e66
ziidonato <zii.donato@outlook.com>
2023-09-09 02:49:08 -0400

    made some benchmarks, got some data, updated workspace

--- benchmarks/benchmark_life.c
+++ benchmarks/benchmark_life.c
@@ -23,6 +23,24 @@ UBENCH_EX(four_gliders, 100_generations)
     }
 }
 
+UBENCH_EX(two_squares, 100_generations)
+{
+    struct grid_data data = {
+            .mutex = PTHREAD_MUTEX_INITIALIZER,
+    };
+
+    UBENCH_DO_BENCHMARK()
+    {
+        data.current_generation = 0;
+        data.grid = init_grid(100);
+        data.current_grid_size = 100;
+        data.total_generations_to_simulate = 100;
+        data.generations_to_simulate = -1;
+        two_squares(data.grid, data.current_grid_size);
+        simulate(&data);
+    }
+}
+
 UBENCH_EX(four_gliders, 1000_generations)
 {
     struct grid_data data = {
@@ -32,11 +50,29 @@ UBENCH_EX(four_gliders, 1000_generations)
     UBENCH_DO_BENCHMARK()
     {
         data.current_generation = 0;
-        data.grid = init_grid(1000);
-        data.current_grid_size = 1000;
+        data.grid = init_grid(100);
+        data.current_grid_size = 100;
         data.total_generations_to_simulate = 1000;
         data.generations_to_simulate = -1;
         four_gliders(data.grid, data.current_grid_size);
+        simulate(&data);
+    }
+}
+
+UBENCH_EX(two_squares, 1000_generations)
+{
+    struct grid_data data = {
+            .mutex = PTHREAD_MUTEX_INITIALIZER,
+    };
+
+    UBENCH_DO_BENCHMARK()
+    {
+        data.current_generation = 0;
+        data.grid = init_grid(100);
+        data.current_grid_size = 100;
+        data.total_generations_to_simulate = 1000;
+        data.generations_to_simulate = -1;
+        two_squares(data.grid, data.current_grid_size);
         simulate(&data);
     }
 }
